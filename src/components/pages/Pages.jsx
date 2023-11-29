11003c00b23117d49f9bddbf92e939f5a8d834e3
BirdBird <birdbirdmurmur@gmail.com>
2023-11-29 17:23:20 +0800

    add header.jsx

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -6,11 +6,13 @@ import {
 } from 'react-router-dom'
 import { Home } from '../home/Home'
 import { About } from './About'
+import { Header } from '../common/Header'
 
 export const Pages = () => {
     return (
         <>
             <Router>
+                <Header />
                 <Switch>
                     <Route exact path='/' component={Home} />
                     <Route exact path='/about' component={About} />
