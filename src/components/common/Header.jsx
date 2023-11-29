b586ff60865f043c9687118f1574934534a00790
BirdBird <birdbirdmurmur@gmail.com>
2023-11-29 17:30:55 +0800

    fix some bug

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
     return (
@@ -6,8 +7,8 @@ export const Header = () => {
             <header>
                 <nav>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
                     </ul>
                 </nav>
             </header>
