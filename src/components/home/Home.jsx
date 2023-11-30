3af8c22b019af6d41e037707bb8473549c1a9988
BirdBird <birdbirdmurmur@gmail.com>
2023-11-30 22:03:40 +0800

    feat: Home rerender(5s)

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export const Home = () => {
+    const [render, setRender] = useState(true)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // setRender(false);
+            setRender(prevState => !prevState);
+        }, 5000);
+
+        return () => clearTimeout(interval);
+    }, [])
+
     return (
         <>
             <section className="home">
-                <div className='homeContainer'>
-                    <h3>Hi, I am BirdBird</h3>
-                    <p>I am a Frontend Developer.</p>
-                </div>
+                {render ? (
+                    <div className='homeContainer'>
+                        <h3>Hi, I am BirdBird</h3>
+                        <p>I am a Frontend Developer.</p>
+                    </div>
+                ) : (
+                    <div className='homeContainer'>
+                        <h3>Hi, I am MurMur</h3>
+                        <p>It's My First Blog by Vite&Vercel.</p>
+                    </div>
+                )}
             </section>
         </>
     )
