5eeca9a65212e3ea0e38fc9a91a476396b983c29
BirdBird <birdbirdmurmur@gmail.com>
2023-11-30 17:32:11 +0800

    change Header CSS

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
+    const [responsive, setResponsive] = useState(false)
     return (
         <>
             <header>
-                <div className="header">
+                <div className="container">
                     <div className='logo'>
                         <Link to="/">
                             <h1>BirdBirdmurmur</h1>
                         </Link>
                     </div>
-                    <div className='menu'>
+                    <div className={responsive ? 'hideMenu' : 'nav'}>
                         <Link to="/">Home</Link>
                         <Link to="/about">About</Link>
                     </div>
+                    <button className='toggle' onClick={() => setResponsive(!responsive)}>
+                        =
+                    </button>
                 </div>
             </header>
         </>
