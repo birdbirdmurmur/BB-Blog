13be056d0d800b6d380bb96306d91bfef1114903
BirdBird <birdbirdmurmur@gmail.com>
2023-12-01 00:57:02 +0800

    update my avatar

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Avatar from '@mui/material/Avatar';
+import BirdBird from '../../../public/Avatar.jpg'
 
 export const About = () => {
     return (
-        <>About</>
+        <>
+            <Avatar alt="BirdBird" src={BirdBird} />
+        </>
     )
 }
