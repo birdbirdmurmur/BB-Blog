591c93d340e9a419f2b76fba3680be858eec08a4
BirdBird <birdbirdmurmur@gmail.com>
2023-12-01 16:05:48 +0800

    update Projects page&CSS

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,6 +2,37 @@ import React from 'react'
 
 export const Projects = () => {
     return (
-        <>Projects</>
+        <>
+            <section className='projects'>
+                <div className='projectCard'>
+                    <img src="https://picsum.photos/300/200/">
+                    </img>
+                    <div className='info'>
+                        <h3>ShoppingCart</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie luctus nisi, vitae rutrum ipsum sagittis eget. Nunc varius bibendum ultricies. Donec finibus dignissim massa eget fringilla. Morbi molestie felis turpis, eget sodales orci imperdiet sodales. Quisque ultricies efficitur placerat. Quisque ornare vulputate nisi. Maecenas dictum nisi non nisi accumsan, vitae tristique libero rhoncus. Fusce vitae dignissim sem, non tincidunt neque.</p>
+                        <ul>
+                            <li>React</li>
+                            <li>Redux</li>
+                            <li>Axios</li>
+                            <li>CSS</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='projectCard'>
+                    <img src="https://picsum.photos/300/200/">
+                    </img>
+                    <div className='info'>
+                        <h3>TodoList</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie luctus nisi, vitae rutrum ipsum sagittis eget. Nunc varius bibendum ultricies. Donec finibus dignissim massa eget fringilla. Morbi molestie felis turpis, eget sodales orci imperdiet sodales. Quisque ultricies efficitur placerat. Quisque ornare vulputate nisi. Maecenas dictum nisi non nisi accumsan, vitae tristique libero rhoncus. Fusce vitae dignissim sem, non tincidunt neque.</p>
+                        <ul>
+                            <li>Html</li>
+                            <li>CSS</li>
+                            <li>Javascript</li>
+                        </ul>
+                    </div>
+                </div>
+                {/* <div className='projectCard'>TBC...</div> */}
+            </section>
+        </>
     )
 }
